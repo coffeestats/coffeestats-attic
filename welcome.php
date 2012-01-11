3d66cd3034edf13a367882e23a5100b0f9a329e4
Florian Baumann <flo@noqqe.de>
2012-01-11 22:50:26 +0100

    Include header / footer

--- welcome.php
+++ welcome.php
@@ -2,6 +2,9 @@
 include('auth/lock.php');
 ?>
 
+<?php include("header.php"); ?>
 
 	Welcome, <strong><?php echo $login_session; ?></strong>!<br />What would you like to do?
-	<a href="auth/logout.php">Ausloggen</a></p>
+	<a href="auth/logout.php">Logout</a></p>
+
+<?php include("footer.php"); ?>
