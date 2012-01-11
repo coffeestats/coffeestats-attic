1ee91f24617c757a741c06142518e8ce18d1c944
Holger Winter <holger.winter@online.de>
2012-01-11 22:48:31 +0100

    added/deleted some files

---
+++ header.php
@@ -0,0 +1,10 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="author" content="F. Baumann, H. Winter" />
+		<meta name="description" content="You can always count on coffee!" />
+		<link rel="stylesheet" href="../css/caffeine.css" type="text/css" media="screen" />
+	</head>
+<body>
