72f99ce0c6eea9358e1fa453d5785050b059dac1
Florian Baumann <flo@noqqe.de>
2012-01-15 16:15:25 +0100

    Your coffee today works

--- statistic.php
+++ statistic.php
@@ -9,24 +9,28 @@ include("auth/config.php");
       google.setOnLoadCallback(drawChart);
       function drawChart() {
         var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Year');
-        data.addColumn('number', 'Sales');
+        data.addColumn('string', 'Hour');
+        data.addColumn('number', 'Coffees');
         data.addRows([
 <?php
-        $sql="select DATE_FORMAT(cdate,'%H') as hour, count(cid) as coffees from cs_coffees where DATE_FORMAT(CURRENT_TIMESTAMP(),'%Y-%m-%d') = DATE_FORMAT(cdate,'%Y-%m-%d') and cuid = '".$_SESSION['login_id']."' GROUP BY DATE_FORMAT(cdate,'%h'); ";
-        $result=mysql_query($sql);
-          while ($row = mysql_fetch_assoc($result)) {
-                    echo ("\t\t['".$row['hour']."', '".$row['coffees']."'],\n");
-          }
-        echo ("\t\t['17','2']");
+                for ( $counter = 0; $counter <= 23; $counter += 1) {
+                  $sql="select '".$counter."' as hour, count(cid) as coffees from cs_coffees where DATE_FORMAT(CURRENT_TIMESTAMP(),'%Y-%m-%d') = DATE_FORMAT(cdate,'%Y-%m-%d') and ( DATE_FORMAT(cdate,'%H') = '".$counter."' or DATE_FORMAT(cdate,'%H') = '0".$counter."') and cuid = '".$_SESSION['login_id']."'; ";
+                  $result=mysql_query($sql);
+                  $row=mysql_fetch_array($result);
+                  echo ("\t\t['".$row['hour']."h', ".$row['coffees']."],\n");
+                }
+                  $sql="select '24' as hour, count(cid) as coffees from cs_coffees where DATE_FORMAT(CURRENT_TIMESTAMP(),'%Y-%m-%d') = DATE_FORMAT(cdate,'%Y-%m-%d') and ( DATE_FORMAT(cdate,'%H') = '24' or DATE_FORMAT(cdate,'%H') = '24') and cuid = '".$_SESSION['login_id']."'; ";
+                  $result=mysql_query($sql);
+                  $row=mysql_fetch_array($result);
+                  echo ("\t\t['".$row['hour']."h', ".$row['coffees']."]");
 ?>
 
         ]);
 
         var options = {
-          width: 400, height: 240,
-          title: 'Company Performance',
-          hAxis: {title: 'Year', titleTextStyle: {color: 'red'}}
+          width: 700, height: 240,
+          title: 'Your Coffee today',
+          hAxis: {title: 'Hour'}
         };
 
         var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
