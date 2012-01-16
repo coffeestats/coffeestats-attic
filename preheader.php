c1a77c67d7c7f908cba9be2e525636cb38181969
Florian Baumann <flo@noqqe.de>
2012-01-16 19:50:45 +0100

    epic layout win

---
+++ preheader.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="author" content="F. Baumann, H. Winter" />
+<meta name="description" content="CoffeeStats | All about coffee" />
+<title>CoffeeStats</title>
+<link rel="stylesheet" type="text/css" href="./../css/precaffeine.css" />
+</head>
+<body>
+
+<div id="wrapper">
+
+	<div id="header">
+        <center><h1><img src="../images/coffee.png">coffeestats.org</h1>
+        You drink, we count!</center>
+	</div>
+	
+	<div id="content"> <!-- end of header.php -->
