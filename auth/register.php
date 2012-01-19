8c94f689cc1cc70ea3340d770d397a3deae16a32
Florian Baumann <flo@noqqe.de>
2012-01-19 20:41:39 +0100

    puh. Alles wegen einer scheiss Zeile.

--- auth/register.php
+++ auth/register.php
@@ -1,16 +1,8 @@
+<form action="" method="post">
 <?php
 	include('config.php');
 	include('../preheader.php');
-?>
-
-		<div class="white-box">
-            <h2>Register</h2>
-			<p>Fill these fields with your data, write down what reCAPTCHA says u and click Register!</p>
-			
-			<form action="" method="post">
-
-<?php
-	require_once('../lib/recaptchalib.php');
+    require_once('../lib/recaptchalib.php');
 
 	// Get a key from https://www.google.com/recaptcha/admin/create
 	$publickey = "6LdnPswSAAAAAFSYLEH9f_b0JcPQ2G1VsOHDmJZY";
@@ -37,16 +29,15 @@
               $password=mysql_real_escape_string(md5(md5($_POST['Password'])));
               $location=mysql_real_escape_string($_POST['Location']);
 
-              echo("$login $email $forename $name $password $location");
-              $sql="SELECT uid FROM cs_users WHERE ulogin='".$login."'";
+              $sql="SELECT uid FROM cs_users WHERE ulogin='".$login."'; ";
               $result=mysql_query($sql);
               $row=mysql_fetch_array($result);
               $count=mysql_num_rows($result);
 
 
               if ($count == 0) { 
-                echo "<br/><b>You got it! Click <a href=\"../index.php\">here</a></b><br/>";
-                echo "<i>Yes. We hate CAPTCHAs too.</i><br/><br/><br/>";
+                echo "<div class=\"white-box\"><h2>You got it! Click <a href=\"../index.php\">here</a></h2>";
+                echo "Yes. We hate CAPTCHAs too.</div>";
                 $sql="INSERT INTO cs_users VALUES ('', '".$login."', '".$email."', '$forename', '$name', '".$password."', NOW(), '".$location."'); ";
                 $result = mysql_query($sql) OR die(mysql_error());
               } 
@@ -60,8 +51,11 @@
         	# set the error code so that we can display it
             $error = $resp->error;
         }
-	}
+    }
 ?>
+        <div class="white-box">
+            <h2>Register</h2>
+			<p>Fill these fields with your data, write down what reCAPTCHA says u and click Register!</p>
             <b>General</b><br/>
 			<input type="text" name="Login" maxlength="20" placeholder="Username" id="register_field_standard" />
 			<input type="password" name="Password" maxlength="20" placeholder="Password" id="register_field_standard" />
@@ -71,19 +65,18 @@
 			<input type="text" name="Forename" maxlength="20" placeholder="Forename" id="register_field_standard" />
 			<input type="text" name="Name" maxlength="20" placeholder="Name" id="register_field_standard" />
 			<input type="text" name="Location" maxlength="20" placeholder="Location" id="register_field_standard" />
-		</div> <!-- end of white-box -->
+		  </div> <!-- end of white-box -->
 		
-		<div class="white-box">
-			<?php
-				echo recaptcha_get_html($publickey, $error);
+		    <div class="white-box">
+            <?php
+			  echo recaptcha_get_html($publickey, $error);
 			?>		
-		</div>
+		    </div>
 		
-		<div class="white-box">
-			<input type="submit" value="Register!" id="register_button_standard" />
-		</div>    
-</form>
-
-<?php 
+		    <div class="white-box">
+			  <input type="submit" value="Register!" id="register_button_standard" />
+		    </div>    
+          </form>
+<?php
 	include('../footer.php'); 
 ?>
