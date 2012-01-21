95e36644bfadb530ad5e59d8789a6d882c266dda
Florian Baumann <flo@noqqe.de>
2012-01-21 16:12:20 +0100

    IT WAS JUST ABOUT AN EMPTY USER FIELD FUCKING ASSHOLE

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php
 	include('auth/lock.php');
-?>
-
-<?php
 	include("header.php"); 
 ?>
 
 		<div class="white-box">
 			<h2>Your charts</h2>
-        <?php echo $_SESSION['login_user']; ?>
 				<p>Here's your todays statistic</p>
-				
 		</div>
 
 <?php 
