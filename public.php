112b9ef31ff423d2050c3381bdeb4cd8c9ef0ce0
Florian Baumann <flo@noqqe.de>
2012-01-22 18:01:38 +0100

    piwik for public.php

--- public.php
+++ public.php
@@ -186,5 +186,17 @@ echo("Coffees total: ".$row['total']."");
 		<div class="white-box">
 		    <p><a href="#">coffeestats.org</a> is a project by <a href="http://noqqe.de">Florian Baumann</a> & <a href="http://savier.n0q.org">Holger Winter</a>. If you like you can follow us on <a href="#">Facebook</a>. We wouldn't be sad if you click "i like", either!</p>
 		</div>
+<!-- Piwik -->
+                  <script type="text/javascript">
+                  var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.n0q.org/" : "http://piwik.n0q.org/");
+                  document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+                  </script><script type="text/javascript">
+  try {
+    var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 6);
+    piwikTracker.trackPageView();
+    piwikTracker.enableLinkTracking();
+  } catch( err ) {}
+    </script><noscript><p><img src="http://piwik.n0q.org/piwik.php?idsite=6" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tracking Code -->
 </body>
 </html>
