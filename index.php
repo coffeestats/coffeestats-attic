6ed54ba8be5c1ee20886b4321e26bf86cd5a6f73
Florian Baumann <flo@noqqe.de>
2013-03-29 19:49:47 +0100

    removed announcment

--- index.php
+++ index.php
@@ -3,18 +3,6 @@
 	include("header.php");
 ?>
 		<div class="white-box">
-			<h2>Welcome to a whole new version of coffeestats!</h2>
-				<p>What does this mean?</p>
-                <ul>
-                <li> We're now using <a href="http://www.chartjs.org/">Chart.js</a>. This saves your privacy a lot more instead of using Google Charts. Web2.0-hyper-HTML5-power and stuff!</li>
-                <li> You are now able to register <a href="http://www.clubmate.de/">Club-Mate</a> (or any other type of mate) as well!</li>
-                <li> New combined graphs for mate and coffee</li> 
-                <li> New userfriendly <a href="plusone">update</a> page </li>
-                <li> Refactored code for performance reasons</li>
-                </ul>
-		</div>
-
-		<div class="white-box">
 			<h2>Whats up?</h2>
 				<p>You like coffee, graphs and nerdy statistics? Well, we do too!</p>
 				<p>If you had a coffee today, or are about to drink one, simply let us know <a href="plusone">here</a> and we'll keep track of it.
