b992751d983b3d20c4df5a89f62d11d962caf7e0
Jan Dittberner <jan@dittberner.info>
2013-05-01 18:15:56 +0200

    simplify and optimize ontherun page
    - check existence of request parameters 'u' and 't' before using them
    - use sprintf to construct SQL queries
    - stop execution when redirecting to auth/login.php
    - fix indent and whitespace
    - simplify JavaScript

--- ontherun.php
+++ ontherun.php
@@ -1,104 +1,127 @@
 <?php
 include('auth/config.php');
-$token=mysql_real_escape_string($_GET['t']);
-$user=mysql_real_escape_string($_GET['u']);
-$ses_sql=mysql_query("select uid, utoken, ulogin from cs_users where ulogin='$user' and utoken='$token'; ");
-$row=mysql_fetch_assoc($ses_sql);
-$token=$row['utoken'];
-$user=$row['ulogin'];
-$profileid=$row['uid'];
-if( (!isset($token)) && (!isset($user)) ) {
-    header("Location: auth/login.php");
+if (array_key_exists('t', $_GET) && array_key_exists('u', $_GET)) {
+    $token=mysql_real_escape_string($_GET['t']);
+    $user=mysql_real_escape_string($_GET['u']);
+    $sql=sprintf(
+        "SELECT uid, utoken, ulogin
+         FROM cs_users
+         WHERE ulogin='%s'
+           AND utoken='%s'",
+        $user, $token);
+    $result=mysql_query($sql);
+    $row=mysql_fetch_assoc($result);
+    $token=$row['utoken'];
+    $user=$row['ulogin'];
+    $profileid=$row['uid'];
 }
+
+if (!isset($token) || !isset($user)) {
+    header("Location: auth/login.php");
+    exit(); // end execution if user or token is not set or not correct
+}
+
 include("preheader.php");
-include('auth/config.php');
 include('lib/antixss.php');
-	if($_POST['coffeetime']) {
-      echo("<div class=\"white-box\">");
-          $coffeedate=mysql_real_escape_string($_POST['coffeetime']);
-          $coffeedate=AntiXSS::setFilter($coffeedate, "whitelist", "string");
-          $sql="SELECT cid, cdate
-                FROM cs_coffees
-                WHERE cdate > (NOW() - INTERVAL '5:00' MINUTE_SECOND)
-                AND (NOW() + INTERVAL '45:00' MINUTE_SECOND) > (cdate + INTERVAL '45' MINUTE_SECOND)
-                AND cuid = '".$profileid."' ;";
-	      $result=mysql_query($sql);
-          $count=mysql_num_rows($result);
-          if($count==0) {
-		      $sql="INSERT INTO cs_coffees VALUES ('','".$profileid."', '".$coffeedate."' ); ";
-		      $result=mysql_query($sql);
-		      echo("Your coffee at ".$coffeedate." was been registered!");
-          } else {
-		      echo("Error: Your last coffee was at least not 5 minutes ago. O_o");
-          }
-    } elseif($_POST['matetime']) {
-      echo("<div class=\"white-box\">");
-          $matedate=mysql_real_escape_string($_POST['matetime']);
-          $matedate=AntiXSS::setFilter($matedate, "whitelist", "string");
-          $sql="SELECT mid, mdate
-                FROM cs_mate
-                WHERE mdate > (NOW() - INTERVAL '5:00' MINUTE_SECOND)
-                AND (NOW() + INTERVAL '45:00' MINUTE_SECOND) > (mdate + INTERVAL '45' MINUTE_SECOND)
-                AND cuid = '".$profileid."' ;";
-          $result=mysql_query($sql);
-          $count=mysql_num_rows($result);
-          if($count==0) {
-              $sql="INSERT INTO cs_mate VALUES ('','".$profileid."', '".$matedate."' ); ";
-              $result=mysql_query($sql);
-              echo("Your mate at ".$matedate." was been registered!");
-          } else {
-              echo("Error: Your last mate was at least not 5 minutes ago. O_o");
-          }
+
+if (array_key_exists('coffeetime', $_POST) && !empty($_POST['coffeetime'])) {
+    echo('<div class="white-box">');
+    $coffeedate=mysql_real_escape_string($_POST['coffeetime']);
+    $coffeedate=AntiXSS::setFilter($coffeedate, "whitelist", "string");
+    $sql=sprintf(
+        "SELECT cid, cdate
+         FROM cs_coffees
+         WHERE cdate > (NOW() - INTERVAL '5:00' MINUTE_SECOND)
+           AND (NOW() + INTERVAL '45:00' MINUTE_SECOND) > (cdate + INTERVAL '45' MINUTE_SECOND)
+           AND cuid = %d",
+        $profileid);
+    $result=mysql_query($sql);
+    $count=mysql_num_rows($result);
+    if ($count==0) {
+        $sql=sprintf(
+            "INSERT INTO cs_coffees (cuid, cdate)
+             VALUES (%d, '%s')",
+            $profileid, $coffeedate);
+        $result=mysql_query($sql);
+        echo("Your coffee at ".$coffeedate." was been registered!");
     }
-      echo("</div>");
+    else {
+        echo("Error: Your last coffee was at least not 5 minutes ago. O_o");
+    }
+}
+elseif (array_key_exists('matetime', $_POST) && !empty($_POST['matetime'])) {
+    echo('<div class="white-box">');
+    $matedate=mysql_real_escape_string($_POST['matetime']);
+    $matedate=AntiXSS::setFilter($matedate, "whitelist", "string");
+    $sql=sprintf(
+        "SELECT mid, mdate
+         FROM cs_mate
+         WHERE mdate > (NOW() - INTERVAL '5:00' MINUTE_SECOND)
+           AND (NOW() + INTERVAL '45:00' MINUTE_SECOND) > (mdate + INTERVAL '45' MINUTE_SECOND)
+           AND cuid = %d",
+        $profileid);
+    $result=mysql_query($sql);
+    $count=mysql_num_rows($result);
+    if ($count==0) {
+        $sql=sprintf(
+            "INSERT INTO cs_mate (cuid, mdate)
+             VALUES (%d, '%s')",
+            $profileid, $matedate);
+        $result=mysql_query($sql);
+        echo("Your mate at ".$matedate." was been registered!");
+    }
+    else {
+        echo("Error: Your last mate was at least not 5 minutes ago. O_o");
+    }
+}
+echo("</div>");
 ?>
 
-    <script type="text/javascript">
+<script type="text/javascript">
+    function pad(n) {
+        return n<10 ? '0'+n : n;
+    }
     function AddPostDataCoffee() {
-      function coffeetime(d){
-        function pad(n){return n<10 ? '0'+n : n}
-        return d.getFullYear()+'-'
-        + pad(d.getMonth()+1)+'-'
-        + pad(d.getDate())+' '
-        + pad(d.getHours())+':'
-        + pad(d.getMinutes())+':'
-        + pad(d.getSeconds())
-      }
-      var d = new Date();
-      document.getElementById('coffeetime').value = coffeetime(d);
-      document.getElementById("coffeeform").submit();
+        function coffeetime(d) {
+            return d.getFullYear() + '-' +
+               pad(d.getMonth() + 1) +'-' +
+               pad(d.getDate()) + ' ' +
+               pad(d.getHours()) + ':' +
+               pad(d.getMinutes()) +':' +
+               pad(d.getSeconds());
+        }
+        var d = new Date();
+        document.getElementById('coffeetime').value = coffeetime(d);
+        document.getElementById("coffeeform").submit();
     }
     function AddPostDataMate() {
-      function coffeetime(d){
-        function pad(n){return n<10 ? '0'+n : n}
-        return d.getFullYear()+'-'
-        + pad(d.getMonth()+1)+'-'
-        + pad(d.getDate())+' '
-        + pad(d.getHours())+':'
-        + pad(d.getMinutes())+':'
-        + pad(d.getSeconds())
-      }
-      var d = new Date();
-      document.getElementById('matetime').value = coffeetime(d);
-      document.getElementById("mateform").submit();
+        function coffeetime(d) {
+            return d.getFullYear() + '-' +
+                pad(d.getMonth() + 1) + '-' +
+                pad(d.getDate()) + ' ' +
+                pad(d.getHours()) + ':' +
+                pad(d.getMinutes()) + ':' +
+                pad(d.getSeconds());
+        }
+        var d = new Date();
+        document.getElementById('matetime').value = coffeetime(d);
+        document.getElementById("mateform").submit();
     }
-    </script>
+</script>
 
-
-		<div class="white-box">
-			<h2>On the run?</h2>
-                <center>
-				<form action="" method="post" id="coffeeform" >
-					<input class="imadecoffee" type="submit" value="Coffee!" id="coffee_plus_one" onclick="AddPostDataCoffee();" /><br />
-                    <input type='hidden' id='coffeetime' name='coffeetime' value='' />
-				</form>
-				<form action="" method="post" id="mateform" >
-					<input class="imademate" type="submit" value="Mate!" id="coffee_plus_one" onclick="AddPostDataMate();" /><br />
-                    <input type='hidden' id='matetime' name='matetime' value='' />
-				</form>
-                </center>
-
-		</div>
+<div class="white-box">
+    <h2>On the run?</h2>
+    <center>
+        <form action="" method="post" id="coffeeform">
+            <input class="imadecoffee" type="submit" value="Coffee!" id="coffee_plus_one" onclick="AddPostDataCoffee();" /><br />
+            <input type='hidden' id='coffeetime' name='coffeetime' value='' />
+        </form>
+        <form action="" method="post" id="mateform">
+            <input class="imademate" type="submit" value="Mate!" id="coffee_plus_one" onclick="AddPostDataMate();" /><br />
+            <input type='hidden' id='matetime' name='matetime' value='' />
+        </form>
+    </center>
+</div>
 <?php
 include('footer.php');
 ?>
