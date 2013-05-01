d031f74444129ddb2facf5f0696531c0177f446c
Jan Dittberner <jan@dittberner.info>
2013-05-01 22:52:33 +0200

    HTML5 validity fix for header
    * css/caffeine.css:
      - add background-image definitions for navigation items
    * header.php:
      - remove duplicate id from ul element
      - remove images from navigation links and replace them with ids that
        trigger matching style elements in caffeine.css
    * index.php:
      - reindent

--- header.php
+++ header.php
@@ -1,33 +1,29 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="author" content="F. Baumann, H. Winter" />
-<meta name="description" content="coffeestats.org | All about coffee" />
-<title>coffeestats.org</title>
-<link rel="stylesheet" type="text/css" href="../css/caffeine.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="author" content="F. Baumann, H. Winter" />
+    <meta name="description" content="coffeestats.org | All about coffee" />
+    <title>coffeestats.org</title>
+    <link rel="stylesheet" type="text/css" href="../css/caffeine.css" />
 </head>
 <body>
-
-<div id="wrapper">
-
-	<div id="header">
-        <h1>coffeestats.org</h1>
-        	<p>...about what keeps you awake at night.</p>
-	</div>
-
-	<div id="content">
-
-		<div class="white-box">
-			<div id="navigation">
-				<ul id="navigation">
-					<li><a href="index"><img src="../images/home.png"> Home</a></li>
-					<li><a href="plusone"><img src="../images/tag.png"> Update</a></li>
-                    <li><a href="profile?u=<?php echo $_SESSION['login_user']; ?>"><img src="../images/profile.png"> Profile</a></li>
-					<li><a href="explore"><img src="../images/list.png"> Explore</a></li>
-					<li><a href="overall"><img src="../images/flask.png"> Overall Stats</a></li>
-					<li><a href="about"><img src="../images/user-4.png"> About</a></li>
-					<li><a href="auth/logout"><img src="../images/out.png"> Logout</a></li>
-				</ul>
-			</div>
-		</div>
+    <div id="wrapper">
+        <div id="header">
+            <h1>coffeestats.org</h1>
+            <p>...about what keeps you awake at night.</p>
+        </div>
+        <div id="content">
+            <div class="white-box">
+                <div id="navigation">
+                    <ul>
+                        <li id="navindex"><a href="index">Home</a></li>
+                        <li id="navplusone"><a href="plusone">Update</a></li>
+                        <li id="navprofile"><a href="profile?u=<?php echo $_SESSION['login_user']; ?>">Profile</a></li>
+                        <li id="navexplore"><a href="explore">Explore</a></li>
+                        <li id="navoverall"><a href="overall">Overall Stats</a></li>
+                        <li id="navabout"><a href="about">About</a></li>
+                        <li id="navlogout"><a href="auth/logout">Logout</a></li>
+                    </ul>
+                </div>
+            </div>
--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php
-	include('auth/lock.php');
-	include("header.php");
+include('auth/lock.php');
+include("header.php");
 ?>
-		<div class="white-box">
-			<h2>Whats up?</h2>
-				<p>You like coffee, graphs and nerdy statistics? Well, we do too!</p>
-				<p>If you had a coffee today, or are about to drink one, simply let us know <a href="plusone">here</a> and we'll keep track of it.
-                You can also <a href="explore">explore</a> a bit and check out the other user's statistics!</p>
-		</div>
-
-		<div class="white-box">
-            <h2>You're not always on a Workstation?</h2>
-            <p>Register a coffee on-the-run! Get your on-the-run url on your <a href="profile?u=<?php echo $_SESSION['login_user']; ?>">profile page</a>.
-                   Simply bookmark the url on your mobile device and you will never forget a coffee =)</p>
-		</div>
-
+<div class="white-box">
+    <h2>Whats up?</h2>
+    <p>You like coffee, graphs and nerdy statistics? Well, we do too!</p>
+    <p>If you had a coffee today, or are about to drink one, simply let us know
+    <a href="plusone">here</a> and we'll keep track of it. You can also
+    <a href="explore">explore</a> a bit and check out the other user's
+    statistics!</p>
+</div>
+<div class="white-box">
+    <h2>You're not always on a Workstation?</h2>
+    <p>Register a coffee on-the-run! Get your on-the-run url on your
+    <a href="profile?u=<?php echo $_SESSION['login_user']; ?>">profile page</a>.
+    Simply bookmark the url on your mobile device and you will never forget a
+    coffee =)</p>
+</div>
 <?php
-	include("footer.php");
+include("footer.php");
 ?>
