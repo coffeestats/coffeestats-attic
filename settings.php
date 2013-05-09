bf9e830a3c0d265412d1053e14e27098641521fc
Jan Dittberner <jan@dittberner.info>
2013-05-09 18:29:01 +0200

    fix mysqli support and other issues in settings
    * css/caffeine.css:
      - define styles for all input fields (of type text, password, submit)
        in forms with class 'coffeeform'
    * fix settings.php:
      - use auth/lock.php uncondionaly because the page is only valid for
        users that are logged in
      - use mysqli properly by calling the right methods and using the
        global $dbconn
      - use switch statements instead of if/elseif constructions to improve
        readability
      - fix sql statements so that both return the same column name
      - use PHPs CSV writing function and an open filehandle instead of
        using custom string concatenation
      - use flash message functionality instead of custom inline HTML
      - use CSS class 'coffeeform' to style form elements and produce valid
        HTML by not generating duplicate id attributes and generating proper
        action attributes on form elements
      - use a hidden form field 'action' to distinguish between operations
      - reindent to 4 leading spaces per level for readability

--- settings.php
+++ settings.php
@@ -1,92 +1,111 @@
 <?php
-if (isset($_SESSION['login_user'])) {
-      include("auth/lock.php");
-} else {
-      include("auth/config.php");
-}
-include("header.php");
+include("auth/lock.php");
 include_once('includes/common.php');
 
 // Export Function
 function export_csv($type) {
-  $content="\"Timestamp\"\n";
-  if($type=="coffee") {
-    $file = 'tmp/coffees-'.$_SESSION['login_user'].'.csv';
-    $sql = sprintf(
-          "SELECT cdate 
-           FROM cs_coffees
-           WHERE cuid = %d",
-           $dbconn->real_escape_string($_SESSION['login_id']));
-  } elseif($type=="mate") {
-    $file = 'tmp/mate-'.$_SESSION['login_user'].'.csv';
-    $sql = sprintf(
-          "SELECT mdate 
-           FROM cs_mate
-           WHERE cuid = %d",
-           $dbconn->real_escape_string($_SESSION['login_id']));
-  }
-  if (($result = $dbconn->query($sql, MYSQLI_USE_RESULT)) === FALSE) {
-    handle_mysql_error();
-  }
-  while ($row = mysql_fetch_array($result)) {
-    $content .= "\"".$row['cdate']."\"\n"; 
-  }
-  file_put_contents($file, $content);
-} 
+    global $dbconn;
+    // TODO: use an absolute base path from configuration for export files
+    switch ($type) {
+    case 'coffee':
+        $file = tempnam(sys_get_temp_dir(), sprintf('coffees-%s', $_SESSION['login_user']));
+        $sql = sprintf(
+            "SELECT cdate AS thedate
+             FROM cs_coffees
+             WHERE cuid = %d",
+            $dbconn->real_escape_string($_SESSION['login_id']));
+        break;
+    case 'mate':
+        $file = tempnam(sys_get_temp_dir(), sprintf('mate-%s', $_SESSION['login_user']));
+        $sql = sprintf(
+            "SELECT mdate AS thedate
+             FROM cs_mate
+             WHERE cuid = %d",
+            $dbconn->real_escape_string($_SESSION['login_id']));
+        break;
+    default:
+        error_log(sprintf('Invalid call to export_csv with type %s', $type));
+        return NULL;
+    }
+    if (($result = $dbconn->query($sql, MYSQLI_USE_RESULT)) === FALSE) {
+        handle_mysql_error();
+    }
+    if (($csvfile = fopen($file, 'w')) !== FALSE) {
+        fputcsv($csvfile, array("Timestamp"));
+        while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            fputcsv($csvfile, array($row['thedate']));
+        }
+        $result->close();
+        fclose($csvfile);
+        return $file;
+    }
+    else {
+        flash("Problem during export", FLASH_ERROR);
+        return NULL;
+    }
+
+}
 
 // Switching between modes
-if(isset($_POST['exportflag'])) {
-  echo("<div class=\"white-box\">");
-  export_csv(coffee);
-  export_csv(mate);
-  echo("</div>");
-} elseif(isset($_POST['updateflag'])) {
-  echo("<div class=\"white-box\">");
-  echo("<h2>Updating...</h2>");
-  echo("</div>");
-} elseif(isset($_POST['deleteflag'])) {
-  echo("<div class=\"white-box\">");
-  echo("<h2>Deleting...</h2>");
-  echo("</div>");
+if (isset($_POST['action'])) {
+    switch ($_POST['action']) {
+    case 'export':
+        $coffeefile = export_csv('coffee');
+        $matefile = export_csv('mate');
+        // TODO: mail CSV files to profile email address
+        flash('Your data has been exported. You will receive an email with two CSV files with your coffee and mate registrations attached.', FLASH_INFO);
+        break;
+    case 'update':
+        flash('Updating ...', FLASH_INFO);
+        break;
+    case 'delete':
+        flash('Deleting ...', FLASH_INFO);
+        break;
+    default:
+        error_log(sprintf('Invalid call wrong POST action %s', $_POST['action']));
+        errorpage('Bad request', 'Your request contained an invalid action', '400 Bad Request');
+    }
 }
+
+include("header.php");
 ?>
-		<div class="white-box">
-          <h2>Update your profile</h2>
-        <p>
+<div class="white-box">
+    <h2>Update your profile</h2>
+    <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" class="coffeeform">
+    <input type="hidden" name="action" value="update" />
+    <p>
         <b>General</b><br/>
-        <input type="text" name="Login" maxlength="20" placeholder="Username" class="register_field_standard" />
-        <input type="password" name="Password" maxlength="20" placeholder="Password" class="register_field_standard" />
-        <input type="text" name="Email" maxlength="50" placeholder="E-Mail" class="register_field_standard" /></p>
-        <p><b>Additional</b><br/>
-        <input type="text" name="Forename" maxlength="20" placeholder="Forename" class="register_field_standard" />
-        <input type="text" name="Name" maxlength="20" placeholder="Name" class="register_field_standard" />
-        <input type="text" name="Location" maxlength="20" placeholder="Location" class="register_field_standard" />
-        </p>
-          
-        </div>
+        <input type="text" name="Login" maxlength="20" placeholder="Username" readonly="readonly" />
+        <input type="password" name="Password" maxlength="20" placeholder="Password" />
+        <input type="text" name="Email" maxlength="50" placeholder="E-Mail" />
+    </p>
+    <p>
+        <b>Additional</b><br/>
+        <input type="text" name="Forename" maxlength="20" placeholder="Forename" />
+        <input type="text" name="Name" maxlength="20" placeholder="Name" />
+        <input type="text" name="Location" maxlength="20" placeholder="Location" />
+    </p>
+    <p><input type="submit" name="submit" value="Update my settings" /></p>
+    </form>
+</div>
 
-	    <div class="white-box">
-          <h2>Your Activity</h2>
-          <p>Your data is yours. You will recieve the csv files via mail.</p>
-           
-				<form action="" method="post" id="exportform">
-					<input class="imadecoffee" type="submit" value="Export, please!" id="coffee_plus_one" />
-                    <input type='hidden' id='exportflag' name='exportflag' value='' />
-				</form>
-        </div>
+<div class="white-box">
+    <h2>Export your Activity</h2>
+    <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" class="coffeeform">
+    <input type="hidden" name="action" value="export" />
+    <p>Your data is yours. You will recieve the csv files via mail.</p>
+    <p><input type="submit" name="submit" value="Export, please!" /></p>
+    </form>
+</div>
 
-
-		<div class="white-box">
-          <h2>Export your data</h2>
-          <p>Your data is yours. You will recieve the csv files via mail.</p>
-           
-				<form action="" method="post" id="exportform">
-					<input class="imadecoffee" type="submit" value="Export, please!" id="coffee_plus_one" />
-                    <input type='hidden' id='exportflag' name='exportflag' value='' />
-				</form>
-        </div>
-
-
+<div class="white-box">
+    <h2>Delete your data</h2>
+    <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" class="coffeeform">
+    <input type="hidden" name="action" value="delete" />
+    <p>Your data is yours. So it is your decission to leave us.</p>
+    <p><input type="submit" name="submit" value="Delete me, please!" /></p>
+    </form>
+</div>
 <?php
-	include("footer.php");
+include("footer.php");
 ?>
