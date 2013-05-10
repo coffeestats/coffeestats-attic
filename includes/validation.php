188f49ddaa98ea5f82253cfad2856f82e10640b8
Jan Dittberner <jan@dittberner.info>
2013-05-10 17:31:10 +0200

    add validation for datetime (addresses #13)
    * includes/validation.php:
      - add validate_datetime to validate combined date/time fields

--- includes/validation.php
+++ includes/validation.php
@@ -2,6 +2,10 @@
 /*
  * Validation functions.
  */
+
+/**
+ * Validate a single password or a pair of passwords.
+ */
 function validate_password($password, $repeat=NULL) {
     if (empty($password)) {
         flash('Password must not be empty!', FLASH_ERROR);
@@ -9,6 +13,21 @@ function validate_password($password, $repeat=NULL) {
     }
     if (($repeat !== NULL) && (strcmp($password, $repeat) !== 0)) {
         flash('Passwords in both fields must be the same!', FLASH_ERROR);
+        return FALSE;
+    }
+    return TRUE;
+}
+
+/**
+ * Validate a combined datetime string.
+ */
+function validate_datetime($datetime) {
+    if (empty($datetime)) {
+        flash('No valid date/time information. Must not be empty!', FLASH_ERROR);
+        return FALSE;
+    }
+    if (!preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}\ [0-9]{2}:[0-9]{2}/', $datetime)) {
+        flash('No valid date/time information. Expected format YYYY-mm-dd HH:MM', FLASH_ERROR);
         return FALSE;
     }
     return TRUE;
