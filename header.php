d55136a8e7a4c8adfddfa7152b9aa45740f9c8bd
Jan Dittberner <jan@dittberner.info>
2013-05-12 00:09:44 +0200

    small HTML improvements
    * footer.php:
      - make imprint link work in subfolders
    * header.php:
      - link h1 headline to index page for convenience
    * imprint.php:
      - rewrap, reindent
      - use p tags appropriately for valid HTML

--- header.php
+++ header.php
@@ -16,7 +16,7 @@ if (strcmp($_SERVER['SCRIPT_FILENAME'], __FILE__) == 0) {
 <body>
     <div id="wrapper">
         <div id="header">
-            <h1>coffeestats.org</h1>
+            <h1><a href="/">coffeestats.org</a></h1>
             <p>...about what keeps you awake at night.</p>
         </div>
         <div id="content">
