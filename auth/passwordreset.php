a95e9a17919b21c2a42fe4027056a0c3896d8cdd
Jan Dittberner <jan@dittberner.info>
2013-05-24 21:24:36 +0200

    add HTML5 validation for passwordreset (addresses #75)
    * auth/passwordreset.php:
      - redirect after failed server side validation
      - start session to show flash messages
      - add required and pattern validation to email field, set type to
        email
      - add JavaScript code to set custom validation messages depending on
        the failed validations
    * css/caffeine.css, css/caffeine.scss:
      - apply the same style to email input fields as for text input fields

--- auth/passwordreset.php
+++ auth/passwordreset.php
@@ -17,8 +17,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         flash('We sent an email with a password reset link if any of our users has an account with the given email address.', FLASH_INFO);
         redirect_to('login');
     }
+    redirect_to($_SERVER['REQUEST_URI']);
 }
 
+if (!isset($_SESSION)) {
+    session_start();
+}
 include_once('../includes/jsvalidation.php');
 include('../header.php');
 ?>
@@ -27,7 +31,7 @@ include('../header.php');
     <p>Please enter the email address that you used when you registered. We will send you an email with a link that you can use to reset your password.</p>
     <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" class="inlineform">
         <p>
-        <input type="text" name="email" id="email" />
+        <input type="email" required pattern="[A-Za-z0-9._%+-]+@[^@]+" name="email" id="email" />
         <input type="submit" name="submit" value="Send me a reset link!" />
         </p>
     </form>
@@ -36,7 +40,15 @@ include('../header.php');
 <?php js_sanitize_email(); ?>
 <script type="text/javascript">
 $(document).ready(function() {
-    $('input#email').focus();
+    $('input#email').focus().bind('invalid', function(event) {
+        if (this.validity.patternMismatch) {
+            this.setCustomValidity('Email address must contain a local part and a domain part separated by an @ sign!');
+        } else if (this.validity.valueMissing) {
+            this.setCustomValidity('Email address must not be empty!');
+        } else {
+            this.setCustomValidity('');
+        }
+    });
 
     $('form').submit(function(event) {
         return sanitize_email('input#email');
