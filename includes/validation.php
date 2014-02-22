af1102d6972706b40d8e87de31414bb2e737aec7
Clemens Lang <neverpanic@gmail.com>
2014-02-22 16:03:26 +0100

    Allow setting flash category in validation funcs
    Setting the flash category to a special value for all validation done
    for the API avoids mixing messages from the website and API. We wouldn't
    want errors from failed sanitation in an API call to show up on the
    website (and vice-versa).

--- includes/validation.php
+++ includes/validation.php
@@ -8,20 +8,20 @@ include_once(sprintf('%s/common.php', dirname(__FILE__)));
  * Validate a single password or a pair of passwords and return a sanitized
  * version of the password.
  */
-function sanitize_password($password, $repeat=NULL, $allowempty=FALSE) {
+function sanitize_password($password, $repeat=NULL, $allowempty=FALSE, $category='system') {
     $password = trim($password);
     if (!$allowempty && empty($password)) {
-        flash('Password must not be empty!', FLASH_ERROR);
+        flash('Password must not be empty!', FLASH_ERROR, $category);
         return FALSE;
     }
     if (!empty($password) && (strlen($password) < 8)) {
-        flash('Password must be at least 8 characters long', FLASH_ERROR);
+        flash('Password must be at least 8 characters long', FLASH_ERROR, $category);
         return FALSE;
     }
     if ($repeat !== NULL) {
         $repeat = trim($repeat);
         if (strcmp($password, $repeat) !== 0) {
-            flash('Passwords in both fields must be the same!', FLASH_ERROR);
+            flash('Passwords in both fields must be the same!', FLASH_ERROR, $category);
             return FALSE;
         }
     }
@@ -31,10 +31,10 @@ function sanitize_password($password, $repeat=NULL, $allowempty=FALSE) {
 /**
  * Validate a given string.
  */
-function sanitize_string($value, $mandatory=TRUE, $name="Field") {
+function sanitize_string($value, $mandatory=TRUE, $name="Field", $category='system') {
     $value = trim($value);
     if (empty($value) && $mandatory) {
-        flash(sprintf('%s must not be empty!', $name), FLASH_ERROR);
+        flash(sprintf('%s must not be empty!', $name), FLASH_ERROR, $category);
         return FALSE;
     }
     return $value;
@@ -44,22 +44,22 @@ function sanitize_string($value, $mandatory=TRUE, $name="Field") {
  * Validate that the given value is not empty. Return the value with stripped
  * whitespace.
  */
-function sanitize_notempty($value, $name) {
-    return sanitize_string($value, TRUE, $name);
+function sanitize_notempty($value, $name, $category='system') {
+    return sanitize_string($value, TRUE, $name, $category);
 }
 
 /**
  * Validate a combined datetime string and return a sanitized version of the
  * datetime value.
  */
-function sanitize_datetime($datetime) {
+function sanitize_datetime($datetime, $category='system') {
     $datetime = trim($datetime);
     if (empty($datetime)) {
-        flash('No valid date/time information. Must not be empty!', FLASH_ERROR);
+        flash('No valid date/time information. Must not be empty!', FLASH_ERROR, $category);
         return FALSE;
     }
     if (!preg_match('/^([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})\ ([0-9]{1,2}):([0-9]{1,2})(|:([0-9]{1,2}))$/', $datetime, $matches)) {
-        flash('No valid date/time information. Expected format YYYY-mm-dd HH:MM', FLASH_ERROR);
+        flash('No valid date/time information. Expected format YYYY-mm-dd HH:MM', FLASH_ERROR, $category);
         return FALSE;
     }
     return sprintf(
@@ -71,20 +71,20 @@ function sanitize_datetime($datetime) {
 /**
  * Validate an e-mail address and return a sanitized version.
  */
-function sanitize_email($email, $optional=FALSE) {
+function sanitize_email($email, $optional=FALSE, $category='system') {
     $email = trim($email);
     if (empty($email)) {
-        flash('Email address must not be empty!', FLASH_ERROR);
+        flash('Email address must not be empty!', FLASH_ERROR, $category);
         return $optional ? "" : FALSE;
     }
     if (!preg_match('/^([A-Za-z0-9._%+-]+)@([^@]+)$/', $email, $matches)) {
-        flash('Email address must contain a local and a domain part separated by one @ sign!', FLASH_ERROR);
+        flash('Email address must contain a local and a domain part separated by one @ sign!', FLASH_ERROR, $category);
         return FALSE;
     }
     $localpart = $matches[1];
     $domainpart = $matches[2];
     if (getmxrr($domainpart, $mxhosts) === FALSE) {
-        flash('Email address must contain a valid domain part!', FLASH_ERROR);
+        flash('Email address must contain a valid domain part!', FLASH_ERROR, $category);
         return FALSE;
     }
     return sprintf("%s@%s", $localpart, $domainpart);
@@ -93,14 +93,14 @@ function sanitize_email($email, $optional=FALSE) {
 /**
  * Validate a username and return a sanitized version.
  */
-function sanitize_username($username) {
+function sanitize_username($username, $category='system') {
     $username = strtolower(trim($username));
     if (empty($username)) {
-        flash('Username must not be empty!', FLASH_ERROR);
+        flash('Username must not be empty!', FLASH_ERROR, $category);
         return FALSE;
     }
     if (!preg_match('/^[a-z][a-z0-9_-]{1,29}$/', $username)) {
-        flash('Invalid username! A username has at least 3 characters, starting with a letter. It may consist of lowercase letters, digits, hypens and underscores.', FLASH_ERROR);
+        flash('Invalid username! A username has at least 3 characters, starting with a letter. It may consist of lowercase letters, digits, hypens and underscores.', FLASH_ERROR, $category);
         return FALSE;
     }
     return $username;
@@ -110,14 +110,14 @@ function sanitize_username($username) {
  * Validate a hexadecimal encoded MD5 hash value and return a sanitized
  * version.
  */
-function sanitize_md5value($value, $name='MD5 value') {
+function sanitize_md5value($value, $name='MD5 value', $category='system') {
     $value = strtolower(trim($value));
     if (empty($value)) {
-        flash(sprintf('%s must not be empty!', $name), FLASH_ERROR);
+        flash(sprintf('%s must not be empty!', $name), FLASH_ERROR, $category);
         return FALSE;
     }
     if (!preg_match('/^[a-f0-9]{32}$/', $value)) {
-        flash(sprintf('Invalid %s', $name), FLASH_ERROR);
+        flash(sprintf('Invalid %s', $name), FLASH_ERROR, $category);
         return FALSE;
     }
     return $value;
